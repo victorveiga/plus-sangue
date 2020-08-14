50059504cef85c7f565a7509a39fd9ad77a04035
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2020-08-14 10:05:48 -0300

    + ajustes na home page

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        return render(request, 'home.html')
+    else:
+        return render(request, 'index.html')    
