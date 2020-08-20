26df98c6681180694f64af35f66ab5169b9bdb2f
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2020-08-20 20:46:55 -0300

    mais ajustes na home page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from doadores.forms import DoadorForm
+from hemonucleos.forms import HemonucleoForm
+
 # Create your views here.
 def home(request):
     if request.user.is_authenticated:
         return render(request, 'home.html')
     else:
-        return render(request, 'index.html')    
+        form_doador = DoadorForm(request.POST or None, request.FILES or None)
+        form_hemocentro = HemonucleoForm(request.POST or None, request.FILES or None)
+
+        if form_doador.is_valid():
+            form_doador.save()
+
+        if form_hemocentro.is_valid():
+            form_hemocentro.save()    
+
+        return render(request, 'index.html', {'form_doador': form_doador, 'form_hemocentro': form_hemocentro})    
